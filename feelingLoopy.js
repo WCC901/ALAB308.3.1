ee6313589cdb45ad0f2d1b2a89a1a17929cc8101
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2023-11-30 23:35:34 -0500

    file feelingLoopy still needs work to figure out.

--- feelingLoopy.js
+++ feelingLoopy.js
@@ -1,4 +1,4 @@
-const data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+const data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
 
 //Empty  variable to hold each row of data
 let cell1 = "";
@@ -6,9 +6,12 @@ let cell2 = "";
 let cell3 = "";
 let cell4 = "";
 
-for (let i = 0; i <= data.indexOf(","); i++) {
-    cell1 = cell1 + data.charAt(i);
+let row1 = "";
+//  Find A way to store each section in
+//  the seperate cell variables
 
+for (let letter in data) {
+    cell1 = cell1 + letter;
 }
 
-console.log(cell1.replace(",", " "));
+console.log(cell1);
